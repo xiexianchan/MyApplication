668f3411562f6f7487e4eb34e613eb7dfc46b447
xiexianchan <894406560@qq.omc>
2017-03-24 14:39:51 +0800

    Hellen2

---
+++ app/src/main/java/com/example/hr/myapplication/WelcomeActivity1.java
@@ -0,0 +1,14 @@
+package com.example.hr.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        //hello
+    }
+}
